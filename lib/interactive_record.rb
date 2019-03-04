befd2506328ac18068c9ecc9589ba5941bad9a8a
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-04 10:41:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,13 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-
   def self.table_name
-    self.to_s.downcase.pluralize
+    "#{self.to_s.downcase}s"
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
 
@@ -49,9 +47,14 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end
 
-
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+  end
 end
